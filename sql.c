1b4385d2821b6eac7cf13657973a8ede0328adb4
Daniel Geelen <daniel.geelen+minidlna@gmail.com>
2017-05-15 21:37:38 +0200

    Implement alternate method of clearing the content database.

--- sql.c
+++ sql.c
@@ -278,3 +278,107 @@ db_upgrade(sqlite3 *db)
 
 	return 0;
 }
+
+int db_clear(sqlite3* db) {
+	if (db == NULL) {
+		DPRINTF(E_WARN, L_DB_SQL, "db is NULL\n");
+		return 0;
+	}
+
+	sqlite3_stmt* stmt = NULL;
+	int result = sqlite3_prepare_v2(
+		db,
+		"SELECT name FROM sqlite_master WHERE type='table';",
+		-1,
+		&stmt,
+		NULL
+	);
+	if(SQLITE_OK != result) {
+		DPRINTF(E_ERROR, L_DB_SQL, "prepare failed: %s\n", sqlite3_errmsg(db));
+		return 0;
+	}
+
+	// Get the list of tables
+	char* tables[32] = {};
+	int table_idx = 0;
+	for(;;) {
+		for(
+			int counter = 0;
+			(SQLITE_BUSY == (result = sqlite3_step(stmt)) || SQLITE_LOCKED == result) && counter < 2;
+			++counter
+		) {
+			/* While SQLITE_BUSY has a built in timeout,
+			 * SQLITE_LOCKED does not, so sleep */
+			if(SQLITE_LOCKED == result)
+				sleep(1);
+		}
+		switch (result) {
+			case SQLITE_ROW: {
+				char* table = NULL;
+				if(sqlite3_column_type(stmt, 0) != SQLITE_NULL) {
+					int len = sqlite3_column_bytes(stmt, 0);
+					if((table = sqlite3_malloc(len + 1)) == NULL) {
+						DPRINTF(E_ERROR, L_DB_SQL, "malloc failed\n");
+						result = SQLITE_NULL;
+						break;
+					}
+					strncpy(table, (const char*)sqlite3_column_text(stmt, 0), len + 1);
+				}
+
+				tables[table_idx++] = table;
+				if(table_idx >= sizeof(tables)) {
+					break;
+				}
+			}; continue;
+
+			case SQLITE_DONE: {
+				/* no rows returned */
+			}; break;
+
+			default: {
+				DPRINTF(E_WARN, L_DB_SQL, "%s: step failed: %d - %s\n%s\n", __func__, result, sqlite3_errmsg(db), "(querying database tables)");
+			}; break;
+		}
+		break;
+	}
+	sqlite3_finalize(stmt);
+
+	// construct SQL statement for dropping all tables in one go
+	char buf[4096] = {};
+	struct string_s sql = {
+		.data = buf,
+		.size = sizeof(buf),
+		.off = 0,
+	};
+	strcatf(&sql, "BEGIN EXCLUSIVE TRANSACTION;");
+	while(table_idx--) {
+		char* table = tables[table_idx];
+		// The sqlite_sequence table is not ours, leave it alone
+		if(table && strcmp(table,"sqlite_sequence")) {
+			strcatf(&sql, "DROP TABLE IF EXISTS %s;", table);
+		}
+		sqlite3_free(table);
+	}
+	if(19 != strcatf(&sql, "COMMIT TRANSACTION;")) {
+		DPRINTF(E_ERROR, L_DB_SQL, "prepare failed, statement string too long!\n");
+		return 0;
+	}
+
+	// Drop all tables if there were no errors thus far.
+	if(SQLITE_DONE == result) {
+		DPRINTF(E_DEBUG, L_DB_SQL, "Dropping all database tables:\n%s\n", sql.data);
+		result = sqlite3_exec(db, sql.data, NULL, NULL, NULL);
+		switch (result) {
+			case SQLITE_OK: {
+				/* dropped ok */
+				DPRINTF(E_INFO, L_DB_SQL, "Succesfully dropped all database tables\n");
+			}; break;
+
+			default: {
+				DPRINTF(E_WARN, L_DB_SQL, "Failed to drop tables: %d (%s)\n", result, sqlite3_errmsg(db));
+			}; break;
+		}
+	}
+
+	return SQLITE_DONE == result;
+}
