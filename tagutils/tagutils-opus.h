b3146df81d2f2cd6ed207c5b009525d6945189a7
Eric Toombs <not@public.net>
2022-05-30 18:08:30 -0400

    Added opus support.
    The changes are identical to the patch provided in https://sourceforge.net/p/minidlna/patches/133/, but without changing the existing behaviour of ogg. That can happen on a different commit.

---
+++ tagutils/tagutils-opus.h
@@ -0,0 +1,6 @@
+//=========================================================================
+// FILENAME	: tagutils-opus.h
+// DESCRIPTION	: Opus metadata reader
+//=========================================================================
+
+static int _get_opusfileinfo(char *filename, struct song_metadata *psong);
